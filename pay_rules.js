ed68c5b2326c0b57775da621cb86cfe47329db36
codinggatsby <gabriellangley2017@gmail.com>
2022-10-02 18:40:43 -0400

    Exercises

---
+++ pay_rules.js
@@ -0,0 +1,10 @@
+// USE STRICT
+
+var payrate = 17.30
+var hoursWorked = 40
+var overtime = (17.30 * 1.5) * 5
+var grossPay = (payrate * hoursWorked) + overtime
+
+//output
+
+console.log("joe got paid " + grossPay);
